b9907277452ed06fdc048d9308a191c2f8444c04
tallner <ctallner@hotmail.com>
2022-03-26 12:39:29 +0100

    Eclipse project setup

---
+++ src/main/java/com/cte/gritwebservices/FinalAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.cte.gritwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalAssignmentApplication.class, args);
+	}
+
+}
