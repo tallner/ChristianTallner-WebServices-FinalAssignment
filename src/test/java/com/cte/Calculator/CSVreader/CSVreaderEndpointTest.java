30545909dfe6f6f09de5221a9141e240e6c96db0
tallner <ctallner@hotmail.com>
2022-03-31 14:35:34 +0200

    testclasses added

---
+++ src/test/java/com/cte/Calculator/CSVreader/CSVreaderEndpointTest.java
@@ -0,0 +1,23 @@
+package com.cte.Calculator.CSVreader;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CSVreaderEndpointTest {
+
+	private String sut;
+	
+	@Before
+	public void setUp() throws Exception {
+		sut = "http://localhost:8080";
+	}
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
