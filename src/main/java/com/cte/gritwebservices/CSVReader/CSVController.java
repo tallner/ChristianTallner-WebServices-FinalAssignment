f4867e2032c0d0743c5f10d707a2441f0ab0b6d2
tallner <ctallner@hotmail.com>
2022-03-31 12:32:52 +0200

    JSON parser implemented
    needs to be restructured

--- src/main/java/com/cte/gritwebservices/CSVReader/CSVController.java
+++ src/main/java/com/cte/gritwebservices/CSVReader/CSVController.java
@@ -1,40 +1,33 @@
-package CSVReader;
+package com.cte.gritwebservices.CSVReader;
 
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.core.io.ClassPathResource;
-
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-
-import javax.print.attribute.standard.Media;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
-
-
 import org.springframework.util.StreamUtils;
+import org.springframework.core.io.ClassPathResource;
 
 
-public class CSVController {
-	
-	@RestController
-	public class dataController implements ErrorController {
+
+
+@RestController
+public class CSVController implements ErrorController {
 
 		@RequestMapping(
-				value = "/csv",
+				value = "/csvtest",
 				method = RequestMethod.GET)
 		public String root() {
 			return "Welcome to my web service!";
 		}
 
 		@RequestMapping(
-				value = "/csv/ShowFile",
+				value = "/csvtestt",
 				method = RequestMethod.GET,
 				produces = MediaType.TEXT_PLAIN_VALUE)
 		public void showCsvFile(HttpServletResponse response) throws IOException {
@@ -45,14 +38,15 @@ public class CSVController {
 		}
 		
 		@RequestMapping(
-				value = "/showCsvFileScanner",
-				method = RequestMethod.GET)
+				value = "/csvtesttt",
+				method = RequestMethod.GET,
+				produces = MediaType.APPLICATION_JSON_VALUE)
 		public String showCsvFileScanner() throws IOException {
 			CSVService myReader = new CSVService();
 			
 			String text = myReader.readFile("sample.csv");
 			return text;
 		}
-	}
+	
 
 }
